e78d17471210413217e24b6f7501fa33a78214cd
konstantinos <boromir674@hotmail.com>
2022-06-11 12:20:52 +0300

    refactor: abstract input sanitization

--- src/cookiecutter_python/backend/sanitization/input_sanitization.py
+++ src/cookiecutter_python/backend/sanitization/input_sanitization.py
@@ -1,21 +1,29 @@
 import logging
 import typing as t
+from functools import cached_property
 
 logger = logging.getLogger(__name__)
 
 SanitizerLike = t.Callable[[t.Any], None]
+ExceptionValue = t.Union[t.Type[Exception], t.Tuple]
+ExceptionsMapValue = t.List[t.Type[Exception]]
 
 
 class Sanitize:
-    exceptions_map: t.Mapping[str, t.Union[t.Type[Exception], t.Tuple]] = {}
-    sanitizers_map: t.Mapping[str, SanitizerLike] = {}
+    sanitizers_map: t.Dict[str, SanitizerLike] = {}
+    exceptions_map: t.Dict[str, ExceptionsMapValue] = {}
 
     def __getitem__(cls, item) -> SanitizerLike:
         return cls.sanitizers_map[item]
 
-    @property
-    def exceptions(self):
-        return self.exceptions_map
+    @cached_property
+    def exceptions(self) -> t.Mapping[str, ExceptionValue]:
+        return {k: v for k, v in self.__iter_exceptions()}
+
+    def __iter_exceptions(self) -> t.Iterator[t.Tuple[str, ExceptionValue]]:
+        for k, v in self.exceptions_map.items():
+            exception: ExceptionValue = v[0] if len(v) == 1 else tuple(v)
+            yield k, exception
 
     @classmethod
     def register_sanitizer(cls, sanitizer_identifier: str):
@@ -29,10 +37,10 @@ class Sanitize:
             """Add the decorated callback to the sanitizers' registry.
 
             Args:
-                func (t.Callable): the callback to add to the registry
+                    func (t.Callable): the callback to add to the registry
 
-            Returns:
-                object: the (sub) class
+                Returns:
+                    object: the (sub) class
             """
             cls.sanitizers_map[sanitizer_identifier] = func
             return func
@@ -56,10 +64,10 @@ class Sanitize:
             Returns:
                 object: the (sub) class
             """
-            if isinstance(cls.exceptions_map.get(sanitizer_identifier, None), tuple):
-                cls.exceptions_map[sanitizer_identifier] = tuple(list(cls.exceptions_map[sanitizer_identifier]) + [exception_class])
+            if sanitizer_identifier not in cls.exceptions_map:
+                cls.exceptions_map[sanitizer_identifier] = [exception_class]
             else:
-                cls.exceptions_map[sanitizer_identifier] = exception_class
+                cls.exceptions_map[sanitizer_identifier].append(exception_class)
             return exception_class
 
         return wrapper
