e5a0ccc67ae5c99e4aa39468cc03e24d2b6e29ab
konstantinos <boromir674@hotmail.com>
2023-12-10 03:56:29 +0200

    refactor(pre_main): simplify 'pre_main' code, add docstring in pre_main funct, and add code comments

--- src/cookiecutter_python/backend/pre_main.py
+++ src/cookiecutter_python/backend/pre_main.py
@@ -1,22 +1,35 @@
 from .context import create_context
+import typing as t
 from .helpers import supported_interpreters
 from .hosting_services import Engine
-from .request import Request
 
 
-def pre_main(**kwargs):
-    request = Request(**kwargs)
+def pre_main(request):
+    """Do preparatory steps Generation process, by settings things as the Template Context.
 
+    Args:
+        **kwargs: Arbitrary keyword arguments.
+    """
+    ## External Services Clients Initialization ## 
+    # clients "how to question" 3rd party web services like pypi, and rtd
+    # making http request to web servers hosting endpoints for APIs
     request.check = Engine.create(request.config_file, request.default_config)
 
+    # initialize the check_results
     request.check_results = request.check.check(request.web_servers)
 
-    interpreters = supported_interpreters(request.config_file, request.no_input)
-    print(f'--- Interpreters type: {type(interpreters)}')
-    print(f'Interpreters: {interpreters}')
-    if interpreters:  # update extra_context
+    # Case 1: NON Interactive Mode <--> `request.no_input == True`
+    #   - if interpreters is None, then no user config file supplied in CLI
+    # Case 2: Interactive Mode <--> `request.no_input == False`
+    #   - always meaningful value, since Interactive Dialog ensures that
+    interpreters: t.Optional[t.Mapping[str, t.Sequence[str]]] = supported_interpreters(request.config_file, request.no_input)
+    # if None, then we are in NON interactive mode, but no User Config, passed in CLI
+
+    if interpreters:  # update cookiecutter extra_context
         # supported interpreters supplied either from yaml or from user's input
-        request.extra_context = create_context(
-            interpreters, extra_context=request.extra_context
+        request.extra_context = dict(
+            request.extra_context or {},
+            **{
+                'interpreters': interpreters,
+            }
         )
-    return request
