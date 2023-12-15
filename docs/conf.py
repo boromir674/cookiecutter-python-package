ff5cf9a63cd21131ef728d59263d7bdd7ed7753d
konstantinos <boromir674@hotmail.com>
2023-12-15 04:51:08 +0200

    release(semantic_version): bump version to 1.8.2

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2022, Konstantinos Lampridis'
 author = 'Konstantinos Lampridis'
 
 # The full version, including alpha/beta/rc tags
-release = '1.8.1'
+release = '1.8.2'
 
 # -- General configuration ---------------------------------------------------
 
