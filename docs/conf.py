a98c3cb5aa3d21bccd2ab71b5a22557648336a54
konstantinos <boromir674@hotmail.com>
2023-12-16 09:48:44 +0200

    chore(semver): bump Semantic Version references in sources

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2022, Konstantinos Lampridis'
 author = 'Konstantinos Lampridis'
 
 # The full version, including alpha/beta/rc tags
-release = '1.8.3'
+release = '1.8.5'
 
 # -- General configuration ---------------------------------------------------
 
