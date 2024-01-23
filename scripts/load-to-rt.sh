2ddfcd8593b04d45257d673591395378075bbec5
konstantinos <boromir674@hotmail.com>
2024-01-23 23:01:10 +0200

    ci(gitops): improve Onboarding Trigger Script

--- scripts/load-to-rt.sh
+++ scripts/load-to-rt.sh
@@ -15,18 +15,22 @@ if [ "$CHANGES_BR" = "main" ] || [ "$CHANGES_BR" = "master" ] || [ "$CHANGES_BR"
   exit 1
 fi
 
+echo "[STEP]: Ensure Upstream is up-to-date with User's Branch"
 git push -u origin HEAD
 
 # GIT OPS
 export tt='board-rt'
 
-git tag "$tt" || (echo "Tag $tt already exists!" && git tag -d "$tt" && echo "Deleted local tag" && git tag "$tt" && echo "Created new tag")
-git push -f origin "$tt" || (echo "Tag $tt already exists!" && git push --delete origin "$tt" && git push origin "$tt")
+echo "[STEP]: Create Tag: $tt"
+git tag "$tt" || (echo "Tag $tt already exists!" && git tag -d "$tt" && echo "Deleted tag ${tt}" && git tag "$tt" && echo "Created tag $tt")
+
+echo "[STEP]: Push Tag: $tt"
+git push origin "$tt" || (echo "Remote Tag $tt already exists!" && git push --delete origin "$tt" && echo "Deleted Remote tag ${tt}" && git push origin "$tt" && echo "Pushed tag $tt")
 
 echo
-echo "Triggered Boarding Worklow!"
-echo
 echo " DONE !!"
+echo
+echo " Triggered Boarding Worklow!"
 
 # NEXT STEPS
 
