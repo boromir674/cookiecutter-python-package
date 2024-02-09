048f7527ef3d8b48e8dfc8dc7041e4656b912fbb
konstantinos <boromir674@hotmail.com>
2024-02-09 16:42:35 +0200

    docs: add docstrings

--- src/cookiecutter_python/backend/hosting_services/exceptions.py
+++ src/cookiecutter_python/backend/hosting_services/exceptions.py
@@ -2,4 +2,5 @@ __all__ = ['ContextVariableDoesNotExist']
 
 
 class ContextVariableDoesNotExist(Exception):
+    """Raised when a Context Variable does not exist, in a dict-like object."""
     pass
