05e00e396cb6ec43cdf5067f69e900727347c36e
konstantinos <boromir674@hotmail.com>
2024-02-09 16:54:12 +0200

    refactor(exception_classifier): more readable code; critical / not-critical classification comments

--- src/cookiecutter_python/exceptions.py
+++ src/cookiecutter_python/exceptions.py
@@ -18,10 +18,23 @@ exceptions = {
 
 
 def error_2_str(error):
-    if isinstance(error, UndefinedVariableInTemplate):
+
+    recognized_non_critical = {UndefinedVariableInTemplate}
+    recognized_critical = set(exceptions['critical']).difference(recognized_non_critical)
+
+    ## Mark NON Critical for program execution ##
+    # Program should potentially be able to handle that exception and proceed
+    if isinstance(error, tuple(recognized_non_critical)):
+        # We 'mark as non-critical', when we find out-of-scope variables (when a
+        # template uses a variable which is not defined in the context),
+        # anticipating the injection of out-of-scope variables programmatically.
         return 'non-critical'
 
-    if isinstance(error,
-        tuple(set(exceptions['critical']).difference(set([UndefinedVariableInTemplate])))
-    ):
+    ## Mark CRITICAL for program execution ##
+    # Program's execution should potentially stop, as unable to handle exception 
+    if isinstance(error, tuple(recognized_critical)):
+        # We Classify as Critical, all our Generator's backend engine Exceptions
+        # which are cokiecutter exceptions, but we exclude
+        # UndefinedVariableInTemplate and we add our Generator's exceptions:
+        # - ContextVariableDoesNotExist
         return 'critical'
