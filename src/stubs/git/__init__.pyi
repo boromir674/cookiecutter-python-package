a01f277d83f7681c445632efcb7a9f5962198725
konstantinos <boromir674@hotmail.com>
2024-02-15 17:21:39 +0200

    ci(build): do Type Checking for all Builds in Reproducable environment

---
+++ src/stubs/git/__init__.pyi
@@ -0,0 +1,8 @@
+import typing as t
+
+class Repo:
+    def __init__(self, folder_path: str, **kwargs: t.Any) -> None: ...
+
+
+class Actor:
+    def __init__(self, name: str, email: str) -> None: ...
