81aa68487fdef6952b122ba08b670cc9e294e2b9
konstantinos <boromir674@hotmail.com>
2024-02-18 01:25:17 +0200

    release(semantic_version): bump version to 1.14.0

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2022, Konstantinos Lampridis'
 author = 'Konstantinos Lampridis'
 
 # The full version, including alpha/beta/rc tags
-release = '1.13.0'
+release = '1.14.0'
 
 # -- General configuration ---------------------------------------------------
 
--- src/cookiecutter_python/__init__.py
+++ src/cookiecutter_python/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.13.0'
+__version__ = '1.14.0'
 
 from . import _logging  # noqa
