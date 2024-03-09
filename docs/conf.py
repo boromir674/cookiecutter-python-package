493a47333831546e806684efcf856c137979fb57
konstantinos <boromir674@hotmail.com>
2024-03-09 15:13:33 +0200

    release(semantic_version): bump version to 2.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2022, Konstantinos Lampridis'
 author = 'Konstantinos Lampridis'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.2'
+release = '2.1.0'
 
 # -- General configuration ---------------------------------------------------
 
