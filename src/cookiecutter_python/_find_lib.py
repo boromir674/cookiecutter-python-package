e758886d83bbe11aaaabbe3fda90c4ebe17bc638
Konstantinos Lampridis <boromir674@hotmail.com>
2025-04-18 22:18:18 +0300

    statisfy mypy

--- src/cookiecutter_python/_find_lib.py
+++ src/cookiecutter_python/_find_lib.py
@@ -11,12 +11,12 @@ __all__ = ['find_lib']
 
 T = t.TypeVar('T')
 
-
+lib_dir: str
 def find_lib(module: t.Optional[str] = None) -> t.Tuple[str, str]:
     if module is None:  # set path as the dir where the invoking code is
         namespace = sys._getframe(2).f_globals  # caller's globals
         # Set as Lib the directory where the invoker module is located at runtime
-        lib_dir: str = path.dirname(path.realpath(namespace['__file__']))
+        lib_dir = path.dirname(path.realpath(namespace['__file__']))
         dotted_lib_path: str = '.'.join(
             Path(lib_dir).relative_to(SRC_DIR).parts
         )  # pragma: no mutate
@@ -29,5 +29,5 @@ def find_lib(module: t.Optional[str] = None) -> t.Tuple[str, str]:
     # Set as Lib the directory where the INPUT module is located at runtime
     # if top-level init is at '/site-packages/some_python_package/__init__.py'
     # then distro_path is '/site-packages/some_python_package'
-    lib_dir: str = str(Path(str(module_object.__file__)).parent)
+    lib_dir = str(Path(str(module_object.__file__)).parent)
     return lib_dir, module
