b8e141c828feb1af7e165c2012c14371ad5b9cb0
Konstantinos Lampridis <boromir674@hotmail.com>
2025-04-19 12:27:37 +0300

    apply black with same config to src code and snapshots code

--- tests/data/snapshots/biskotaki-no-input/scripts/parse_version.py
+++ tests/data/snapshots/biskotaki-no-input/scripts/parse_version.py
@@ -23,8 +23,7 @@ MatchData = t.Union[
 
 
 DEMO_SECTION: str = (
-    "[tool.software-release]\nversion_variable = "
-    "src/package_name/__init__.py:__version__"
+    "[tool.software-release]\nversion_variable = " "src/package_name/__init__.py:__version__"
 )
 TOML = 'pyproject.toml'
 
